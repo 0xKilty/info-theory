065215cf2bf048a855e198968f44c4a669501b87
0xKilty <iankilty1@gmail.com>
2024-04-28 21:13:50 -0600

    ware

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
-use clap::{App, Arg};
+use clap::{App, Arg, ArgGroup};
 use std::error::Error;
 use std::fs;
 use std::io;
 
-fn get_file_names() -> Result<(String, Option<String>), Box<dyn Error>> {
+#[derive(Debug)]
+enum OperationMode {
+    Compression,
+    Decompression,
+}
+
+fn get_flags() -> Result<(String, String, OperationMode), Box<dyn Error>> {
     let matches = App::new("Rust file compressor")
         .version("1.0")
         .author("Ian Kilty")
@@ -13,37 +19,85 @@ fn get_file_names() -> Result<(String, Option<String>), Box<dyn Error>> {
                 .short('i')
                 .long("input")
                 .takes_value(true)
-                .required(true),
+                .required(true)
         )
         .arg(
             Arg::new("output")
                 .short('o')
                 .long("output")
-                .takes_value(true),
+                .takes_value(true)
+        )
+        .arg(
+            Arg::new("compression")
+                .short('c')
+                .long("compress")
+                .conflicts_with("decompression")
+        )
+        .arg(
+            Arg::new("decompression")
+                .short('d')
+                .long("decompress")
+                .conflicts_with("compression")
+        )
+        .group(
+            ArgGroup::new("mode")
+                .args(&["compression", "decompression"])
+                .required(true)
         )
         .get_matches();
 
     let input_file = matches.value_of("input").unwrap().to_string();
-    let output_file = matches.value_of("output").map(String::from);
+    let output_file = matches
+        .value_of("output")
+        .map(|o| o.to_string())
+        .unwrap_or_else(|| format!("{}_compressed", input_file));
 
-    Ok((input_file, output_file))
+    let mode = if matches.is_present("compression") {
+        OperationMode::Compression
+    } else {
+        OperationMode::Decompression
+    };
+
+    Ok((input_file, output_file, mode))
+}
+
+fn read_file_and_handle<F>(file_path: &str, handle_contents: F)
+where
+    F: FnOnce(String),
+{
+    match fs::read_to_string(file_path) {
+        Ok(contents) => {
+            handle_contents(contents);
+        }
+        Err(err) => {
+            eprintln!("Error reading input file: {}", err);
+            std::process::exit(1);
+        }
+    }
+}
+
+fn compress(input_file: String, output_file: String) {
+    read_file_and_handle(&input_file, |contents| {
+        println!("File contents:\n{}", contents);
+        // Add your compression logic here
+    });
+}
+
+fn decompress(input_file: String, output_file: String) {
+    read_file_and_handle(&input_file, |contents| {
+        println!("File contents:\n{}", contents);
+        // Add your compression logic here
+    });
 }
 
 fn main() -> io::Result<()> {
-    match get_file_names() {
-        Ok((input_file, output_file)) => {
-            if let Some(output_file) = output_file {
-                println!("Output file: {}", output_file);
-
-                if let Ok(contents) = fs::read_to_string(input_file) {
-                    println!("Input file contents: {}", contents);
-                } else {
-                    eprintln!("Error reading input file");
-                    std::process::exit(1);
-                }
-            } else {
-                println!("No output file specified.");
+    match get_flags() {
+        Ok((input_file, output_file, mode)) => {
+            match mode {
+                OperationMode::Compression => { compress(input_file, output_file); }
+                OperationMode::Decompression => { decompress(input_file, output_file); }
             }
+            
         }
         Err(err) => {
             eprintln!("Error: {}", err);
